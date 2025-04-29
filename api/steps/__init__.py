f611e3a9f5ebfd7b166f1084fbf9c810b611079b
Daniel Dominguez <daniel@Daniels-MBP.attlocal.net>
2025-04-28 20:46:41 -0500

    chore: convert score controller to chain

--- api/steps/__init__.py
+++ api/steps/__init__.py
@@ -3,6 +3,7 @@ from .embed_query import embed_query
 from .parse_query import parse_query
 from .user_query import clean_user_query
 from .pinecone import pinecone_query
+from .score_query import score_query
 from .db_query import db_query
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "parse_query",
     "clean_user_query",
     "pinecone_query",
+    "score_query",
     "db_query"
 ]
